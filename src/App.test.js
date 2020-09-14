665ed275f1630b71549a38f2b1ba155f0c14f6c3
alsong <alsongdunstan2@gmail.com>
2020-09-14 23:22:00 +0300

    cleaned code according to codacy

--- src/App.test.js
+++ src/App.test.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import App from './App'
-import { findByTestAttr } from './utils'
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import { findByTestAttr } from './utils';
 
 const setUp = (props = {}) => {
-  const component = shallow(<App {...props} />)
-  return component
-}
+  const component = shallow(<App {...props} />);
+  return component;
+};
 
 
 describe('App Component', () => {
 
-  let component
+  let component;
   beforeEach(() => {
-    component = setUp()
-  })
+    component = setUp();
+  });
 
   test('should render without errors', () => {
-    const wrapper = findByTestAttr(component, 'AppComponent')
-    expect(wrapper.length).toBe(1)
-  })
+    const wrapper = findByTestAttr(component, 'AppComponent');
+    expect(wrapper.length).toBe(1);
+  });
 
   test('should render the header container without errors', () => {
-     const wrapper = findByTestAttr(component, 'App-header-container')
-     expect(wrapper.length).toBe(1)
-  })
-  
+    const wrapper = findByTestAttr(component, 'App-header-container');
+    expect(wrapper.length).toBe(1);
+  });
 
-})
+
+});
