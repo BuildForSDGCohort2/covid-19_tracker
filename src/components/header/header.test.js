277ecbdfe70f0736b098f756ad0bbc240fd0d039
alsong <alsongdunstan2@gmail.com>
2020-09-16 22:53:14 +0300

    wrote unit tests for the header component

---
+++ src/components/header/header.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from './index';
+import { shallow } from "enzyme";
+import { findByTestAttr } from "./../../utils";
+
+const setUp = (props = {}) => {
+    const component = shallow(<Header {...props} />);
+    return component;
+};
+
+describe("Header Component", () => {
+    let component;
+    beforeEach(() => {
+        component = setUp();
+    });
+
+    test("should render form control without errors", () => {
+        const wrapper = findByTestAttr(component, "formcontrol");
+        expect(wrapper.length).toBe(1);
+      });
+    
+      test("should render the dropdown element without errors", () => {
+        const wrapper = findByTestAttr(component, "dropdown");
+        expect(wrapper.length).toBe(1);
+      });
+
+      test("should render the menuitem element without errors", () => {
+        const wrapper = findByTestAttr(component, "menuitem");
+        expect(wrapper.length).toBe(1);
+      });
+})
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -30,9 +30,9 @@ const Header = () => {
     return (
         <>
             <h1>COVID-19 TRACKER</h1>
-            <FormControl className="app__dropdown">
-                <Select variant="outlined" onChange={onCountryChange} value={country}>
-                    <MenuItem value="worldwide">Worldwide</MenuItem>
+            <FormControl className="app__dropdown" data-test="formcontrol">
+                <Select variant="outlined" onChange={onCountryChange} value={country} data-test="dropdown">
+                    <MenuItem value="worldwide" data-test="menuitem">Worldwide</MenuItem>
                     {/* Loop through all the countries and show a dropdown list of the options */}
                     {countries.map((country) => (
                         <MenuItem value={country.value}>{country.name}</MenuItem>
