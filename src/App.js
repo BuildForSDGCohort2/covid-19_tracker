b8d0952604f5ba60d48fcdc436d06768d3f8c07a
alsong <alsongdunstan2@gmail.com>
2020-09-18 11:43:10 +0300

    Added the infobox component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/header";
+import InfoBox from "./components/infobox";
 
 function App() {
   return (
     <div className="App" data-test="AppComponent">
       <div className="app__header" data-test="App-header-container">
         <Header />
+      </div>
+      <div className="app_stats" data-test="app_stats_container">
+        <InfoBox title="Coronavirus cases" total={200} cases={122}/>
+        <InfoBox title="Recovered" total={300} cases={22}/>
+        <InfoBox title="Deaths" total={30} cases={67}/>
       </div>
     </div>
   );
